51c72c34475d11ea7a704d3b6b69744ea40b6f57
João Batista <68834412+juniorjblp@users.noreply.github.com>
2022-03-10 14:14:30 -0300

    Update Cep.cs

--- Cep.cs
+++ Cep.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +17,7 @@ namespace Domain.ValueObjects
         {
             _onlyDigitsCep = string.Empty;
 
-            if (value == null)
+            if (value == null || value.Length != 8)
             {
                 IsValid = false;
                 return;
